48eafaab825b50ef0a2c1b1a53454ad4c10a7145
Qoushik <qstar.inc@gmail.com>
2024-04-23 23:54:52 +0600

    update to 1.1

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# 1.1 (2024-04-23)
+- Added separate system and the notification is only viewable on the Main Menu.
+
+# 1.0 (2024-04-21)
+- Initial Release
--- Mod.cs
+++ Mod.cs
@@ -1,18 +1,20 @@
 ﻿using Colossal.Logging;
-using Game.PSI;
+using Colossal.Serialization.Entities;
 using Game;
 using Game.Modding;
+using Game.PSI;
 using Game.SceneFlow;
-using System.Threading.Tasks;
+using Unity.Entities;
 
 namespace SimpleModChecker
 {
     public class Mod : IMod
     {
         public const string ModName = "Simple Mod Checker";
+
+        public SimpleModChecker _simpleModChecker;
+
         public static ILog log = LogManager.GetLogger($"{nameof(SimpleModChecker)}.{nameof(Mod)}").SetShowsErrorsInUI(false);
-        //private SimpleModCheckerSetting m_Setting;
-        private int count;
         public void OnLoad(UpdateSystem updateSystem)
         {
             log.Info(nameof(OnLoad));
@@ -20,48 +22,70 @@ namespace SimpleModChecker
             if (GameManager.instance.modManager.TryGetExecutableAsset(this, out var asset))
                 log.Info($"Current mod asset at {asset.path}");
 
-            //m_Setting = new SimpleModCheckerSetting(this);
-            //m_Setting.RegisterInOptionsUI();
-            //GameManager.instance.localizationManager.AddSource("en-US", new LocaleEN(m_Setting));
-            //m_Setting.DummyItem = false;
-            //AssetDatabase.global.LoadSettings(nameof(SimpleModChecker), m_Setting, new SimpleModCheckerSetting(this));
+            _simpleModChecker = new SimpleModChecker(this);
+            World.DefaultGameObjectInjectionWorld.AddSystemManaged(_simpleModChecker);
+        }
 
+        public void OnDispose()
+        {
+            log.Info(nameof(OnDispose));
+        }
+    }
+
+    public partial class SimpleModChecker : GameSystemBase
+    {
+        public Mod _mod;
+        private int count;
+
+        public SimpleModChecker(Mod mod)
+        {
+            _mod = mod;
+        }
+
+        protected override void OnCreate()
+        {
+            base.OnCreate();
+            CheckMod();
+        }
+
+        protected override void OnGameLoadingComplete(Purpose purpose, GameMode mode)
+        {
+            base.OnGameLoadingComplete(purpose, mode);
+
+            if (mode.IsGameOrEditor())
+            {
+                RemoveNotification();
+            }
+            else
+            {
+                SendNotification(count);
+            }
+        }
+        protected override void OnUpdate()
+        {
+        }
+
+        private void CheckMod()
+        {
             count = 0;
 
             foreach (var modInfo in GameManager.instance.modManager)
             {
                 count += 1;
-                log.Info($"Loaded: {modInfo.asset.name}");
+                Mod.log.Info($"Loaded: {modInfo.asset.name}");
             }
-            log.Info($"Total mod(s): {count}");
-            //bool showNotif = m_Setting.Toggle;
-            //log.Info($"showNotif = '{showNotif}'");
-
-            //if (showNotif)
-            //{
-            //    log.Info("Showing Notification");
+            Mod.log.Info($"Total mod(s): {count}");
             SendNotification(count);
-            //}
-            //else
-            //{
-            //    log.Info("Hiding Notification");
-            //    RemoveNotification();
-            //}
         }
 
-        private static async void SendNotification(int count)
+        private void SendNotification(int count)
         {
-            
-            while (!GameManager.instance.modManager.isInitialized)
-            {
-                await Task.Delay(5);
-            }
-
             var modstext = "mod";
             if (count < 2)
             {
                 modstext += "";
-            } else
+            }
+            else
             {
                 modstext += "s";
             }
@@ -71,19 +95,10 @@ namespace SimpleModChecker
                         text: $"Loaded {count} {modstext}");
         }
 
-        //private static void RemoveNotification()
-        //{
-        //    NotificationSystem.Pop("mod-check");
-        //}
-
-        public void OnDispose()
+        private void RemoveNotification()
         {
-            log.Info(nameof(OnDispose));
-            //if (m_Setting != null)
-            //{
-            //    m_Setting.UnregisterInOptionsUI();
-            //    m_Setting = null;
-            //}
+            NotificationSystem.Pop("mod-check");
         }
+
     }
 }
